edd0851cf6da6d1a8c0fc22a0426c07e1a089ab8
will-da-pro <william@dolier.net>
2022-10-06 23:29:46 +0100

    Drivebase script

--- main.py
+++ main.py
@@ -1,27 +1,13 @@
 import cv2
 import numpy as np
+import RPi.GPIO as gpio
+import drive
 
-#img = cv2.imread('./tiles/JPEG/7. Dead End.jpg', cv2.IMREAD_UNCHANGED)
-img = cv2.imread('./tiles/JPEG/1. Straight.jpg', cv2.IMREAD_UNCHANGED)
-#img = np.full((1000,1000,3), 12, np.uint8)
-#convert img to grey
-#if img
-try:    
-    img_grey = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-except:
-    print("error")
-    img_grey = cv2.cvtColor(img, cv2.COLOR_GRAY)
-#set a thresh
-thresh = 1500
-#get threshold image
-ret,thresh_img = cv2.threshold(img_grey, thresh, 255, cv2.THRESH_BINARY)
-contours, hierarchy = cv2.findContours(thresh_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-#create an empty image for contours
-img_contours = np.zeros(img.shape)
-# draw the contours on the empty image
-cv2.drawContours(img_contours, contours, -1, (0,255,0), 3)
-#save image
-cv2.imshow('Contours',img_contours) 
-cv2.imshow('Grey', img_grey)
+img = cv2.imread("/home/admin/robocup_2023/straight.png")
+cv2.imshow("Straight", img)
+print("Window Shown")
 cv2.waitKey()
+
+robot = drive.driveBase(1, 2, 3, 4)
+
+robot.drive(10, 2)
