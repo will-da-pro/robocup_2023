be63fcf485083bd7507d81d65c668eff372975a3
TheRustedBeasty <100879190+TheRustedBeasty@users.noreply.github.com>
2023-05-21 10:52:34 +1000

    added drive into motor driver

--- robotScripts/motorDriver.py
+++ robotScripts/motorDriver.py
@@ -1,9 +1,10 @@
 import RPi.GPIO as GPIO
+from time import sleep
 
 class Motors:
-    def __init__(self, speedL, speedR):
-        self.speedL = speedL
-        self.speedR = speedR
+    def __init__(self, speed, turnAngle):
+        self.speed = speed
+        self.turnAngle = turnAngle
 
         global in1 
         global in2
@@ -35,12 +36,14 @@ class Motors:
         #GPIO.PWM(en2,1000)
 
     def moveLeft(self):
-        GPIO.PWM(en1,1000).ChangeDutyCycle(self.speedL) #sets speed - self.speed must be 0-100
+        speedL = (self.speed + self.turnAngle) / 100
 
-        if self.speedL > 0: #turns the motor forward
+        GPIO.PWM(en1,1000).ChangeDutyCycle(speedL)
+        
+        if speedL > 0: #right/backward
             GPIO.output(in1,GPIO.HIGH)
             GPIO.output(in2,GPIO.LOW)
-        elif self.direction < 0: #turns the motor backward
+        elif speedL < 0: #left/forward
             GPIO.output(in1,GPIO.LOW)
             GPIO.output(in2,GPIO.HIGH)
         else:
@@ -48,14 +51,50 @@ class Motors:
             GPIO.output(in2,GPIO.LOW)
 
     def moveRight(self):
-        GPIO.PWM(en2,1000).ChangeDutyCycle(self.speedR) 
+        speedR = (self.speed - self.turnAngle) / 100
 
-        if self.speedR > 0:
+        GPIO.PWM(en2,1000).ChangeDutyCycle(speedR)
+
+        if speedR > 0: #right/forward
             GPIO.output(in3,GPIO.HIGH)
             GPIO.output(in4,GPIO.LOW)
-        elif self.direction < 0:
+        elif speedR < 0: #left/backward
             GPIO.output(in3,GPIO.LOW)
             GPIO.output(in4,GPIO.HIGH)
-        else:
+        else: #stop
             GPIO.output(in3,GPIO.LOW)
             GPIO.output(in4,GPIO.LOW)
+
+    def forward(self, time, speed):
+        GPIO.PWM(en1,1000).ChangeDutyCycle(speed)
+        GPIO.PWM(en2,1000).ChangeDutyCycle(speed)
+
+        GPIO.output(in3,GPIO.HIGH)
+        GPIO.output(in4,GPIO.LOW)
+        GPIO.output(in1,GPIO.LOW)
+        GPIO.output(in2,GPIO.HIGH)
+        sleep(time)
+        GPIO.output(in3,GPIO.LOW)
+        GPIO.output(in4,GPIO.LOW)
+        GPIO.output(in1,GPIO.LOW)
+        GPIO.output(in2,GPIO.LOW)
+
+    def backward(self, time, speed):
+        GPIO.PWM(en1,1000).ChangeDutyCycle(speed)
+        GPIO.PWM(en2,1000).ChangeDutyCycle(speed)
+
+        GPIO.output(in3,GPIO.LOW)
+        GPIO.output(in4,GPIO.HIGH)
+        GPIO.output(in1,GPIO.HIGH)
+        GPIO.output(in2,GPIO.LOW)
+        sleep(time)
+        GPIO.output(in3,GPIO.LOW)
+        GPIO.output(in4,GPIO.LOW)
+        GPIO.output(in1,GPIO.LOW)
+        GPIO.output(in2,GPIO.LOW)
+
+    def stop(self):
+        GPIO.output(in1,GPIO.LOW)
+        GPIO.output(in2,GPIO.LOW)
+        GPIO.output(in3,GPIO.LOW)
+        GPIO.output(in4,GPIO.LOW)
