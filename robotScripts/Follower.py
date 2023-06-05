66ad85615021fbfed074df3bf7091dbe2bf02fab
TheRustedBeasty <100879190+TheRustedBeasty@users.noreply.github.com>
2023-06-06 08:40:03 +1000

    sped up cv2.video capture

--- robotScripts/Follower.py
+++ robotScripts/Follower.py
@@ -2,22 +2,39 @@ import cv2
 import numpy as np
 from time import sleep
 #from motorDriver import Motors
+import warnings
+import time
+
+start_time = [None]
+
+def stopwatch_start():
+    start_time[0] = time.time()
+
+def stopwatch_stop():
+    execution_time = time.time() - start_time[0]
+    start_time[0] = None
+    return execution_time
+
+warnings.filterwarnings("ignore")
 
 class LineFollower:
     def __init__(self):
         pass
     
-    def follow(self, frame) -> None:
+    def follow(self, cap: cv2.VideoCapture) -> None:
+        ###
         frameWidth = 1358
         
+        ret, frame = cap.read()
         self.error = 0
-            
+
+        ###0.22 seconds
+
         #roi = frame[100:158,0:255]
         green = cv2.inRange(frame,(0,80,0),(70,255,60))
         greenContours,_ = cv2.findContours(green, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
         line = cv2.inRange(frame,(0,0,0),(40,40,40))
-    
         #line = cv2.GaussianBlur (line, (5,5),0)
         #add erode and dilate
 
@@ -30,9 +47,9 @@ class LineFollower:
             greenDetected = False
     
         lineContours,_ = cv2.findContours(line, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-            
         if len(lineContours) == 0:
             print("no line")
+            error = 0
         else:
             largestContour = max(lineContours,key=cv2.contourArea)
             x,y,w,h = cv2.boundingRect(largestContour)
@@ -43,12 +60,10 @@ class LineFollower:
             x = int(m['m10']/m['m00'])
             y = int(m['m01']/m['m00'])
             error = (x-(frameWidth/2))/679*100
-            error -= 99
             print("error = "+str(error))
             cv2.circle(frame,(x,y),5,(0,0,255),-1)
         else:
             error = 0
-            
         if greenDetected == True:
             #Motors.stop()
             if error > 0:
@@ -60,8 +75,10 @@ class LineFollower:
             
         return error
 
-
 if __name__ == '__main__':
     follower = LineFollower()
+    stopwatch_start()
     cap = cv2.VideoCapture(0)
+    time = stopwatch_stop()
+    print(f"Execution time: {time:.2f} seconds")
     follower.follow(cap)
--- robotScripts/main.py
+++ robotScripts/main.py
@@ -1,26 +1,26 @@
 from Follower import LineFollower
-from motorDriver import Motors
+#from motorDriver import Motors
 from PidCalc import PID
 import cv2
-from time import sleep
 
 lastError = 0
 pastErrors = 0
-motorsInit = Motors()
+#motorsInit = Motors()
+cap = cv2.VideoCapture(0)
 while True:
     #error calc
     follower = LineFollower()
-    error = follower.follow(cv2.VideoCapture(0))
+    error = follower.follow(cap)
     #pid calc
     #print(error)
-    pid = PID(error,8,0.5,2,lastError,pastErrors)#change 1's to multipliers
+    pid = PID(error,2,0.05,0.2,lastError,pastErrors)#change 1's to multipliers
     turnRate = pid.calcTurnRate() 
     pastErrors = error + lastError
     lastError = error
     #print(turnRate)
     #motor output
-    motorsInit.drive(100,100)
-    sleep(1)
+    #motorsInit.drive(100,100)
+    #sleep(1)
     #water tower
 
     #rescue
