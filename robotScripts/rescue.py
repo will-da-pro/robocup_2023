760ab545bde6e648a299a548e0d9de8406de735f
will-da-pro <william@dolier.net>
2023-06-08 14:06:12 +1000

    added some rescue pseudocode

---
+++ robotScripts/rescue.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+import gpiozero
+
+def rescue():
+    # Go forwards
+    # rotate left approx 120 degrees
+    # Engage huskylens
+    # Start rotating right
+    # Start looking for can
+    # If can is detected, stop rotating
+    # Rotate until can is in center
+    # move forwards and grab can
+    # move back
+    # Rotate until facing block
+    # Move forwards to block
+    # Lift can up
+    #
+    pass
