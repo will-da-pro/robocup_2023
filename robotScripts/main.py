266768c6614d54bb024121617617240fe87ae7cb
TheRustedBeasty <100879190+TheRustedBeasty@users.noreply.github.com>
2023-06-12 19:05:20 +1000

    removed commented lines

--- robotScripts/main.py
+++ robotScripts/main.py
@@ -1,11 +1,11 @@
 from Follower import LineFollower
-#from motorDriver import Motors
+from motorDriver import Motors
 from PidCalc import PID
 import cv2
 
 lastError = 0
 pastErrors = 0
-#motorsInit = Motors()
+motorsInit = Motors()
 cap = cv2.VideoCapture(0)
 while True:
     #error calc
@@ -13,15 +13,13 @@ while True:
     error = follower.follow(cap)
     cv2.imshow("frame", follower.frame)
     #pid calc
-    #print(error)
     pid = PID(error,2,0.05,0.2,lastError,pastErrors)#change 1's to multipliers
     turnRate = pid.calcTurnRate() 
     pastErrors = error + lastError
     lastError = error
-    #print(turnRate)
+    print(turnRate)
     #motor output
-    #motorsInit.drive(100,100)
-    #sleep(1)
+    motorsInit.drive(100,turnRate)
     #water tower
 
     #rescue
