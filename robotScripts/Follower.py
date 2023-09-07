178c9c7786f350cdd2f25e6d0ae516b429b8f539
TheRustedBeasty <100879190+TheRustedBeasty@users.noreply.github.com>
2023-09-07 13:00:31 +1000

    nvm

--- robotScripts/Follower.py
+++ robotScripts/Follower.py
@@ -1,69 +1,10 @@
 import cv2
 import numpy as np
-from picamera.array import PiRGBArray
-from picamera import PiCamera
-from threading import Thread
 from time import sleep
 #from motorDriver import Motors
 import warnings
 import math
 
-class PiVideoStream:
-	def __init__(self, resolution=(480, 240), framerate=41, **kwargs):
-		# initialize the camera
-		self.camera = PiCamera()
-
-		# set camera parameters
-		self.camera.resolution = resolution #480x240
-		self.camera.framerate = framerate
-		self.camera.sensor_mode = 6
-		self.camera.shutter_speed = 7000
-		self.camera.exposure_mode = 'off'
-		
-		# set optional camera parameters (refer to PiCamera docs)
-		for (arg, value) in kwargs.items():
-			setattr(self.camera, arg, value)
-
-		# initialize the stream
-		self.rawCapture = PiRGBArray(self.camera, size=resolution)
-		self.stream = self.camera.capture_continuous(self.rawCapture,
-			format="bgr", use_video_port=True)
-
-		# initialize the frame and the variable used to indicate
-		# if the thread should be stopped
-		self.frame = None
-		self.stopped = False
-
-	def start(self):
-		# start the thread to read frames from the video stream
-		t = Thread(target=self.update, args=())
-		t.daemon = True
-		t.start()
-		return self
-
-	def update(self):
-		# keep looping infinitely until the thread is stopped
-		for f in self.stream:
-			# grab the frame from the stream and clear the stream in
-			# preparation for the next frame
-			self.frame = f.array
-			self.rawCapture.truncate(0)
-
-			# if the thread indicator variable is set, stop the thread
-			# and resource camera resources
-			if self.stopped:
-				self.stream.close()
-				self.rawCapture.close()
-				self.camera.close()
-				return
-
-	def read(self):
-		# return the frame most recently read
-		return self.frame
-
-	def stop(self):
-		# indicate that the thread should be stopped
-		self.stopped = True
 
 class LineFollower:
     def __init__(self):
