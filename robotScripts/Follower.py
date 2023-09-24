1a990aafa45f85b883e1e04e9709d51cff0fc71e
TheRustedBeasty <100879190+TheRustedBeasty@users.noreply.github.com>
2023-09-24 18:16:56 +1000

    fixed follower logic (havent tested)

--- robotScripts/Follower.py
+++ robotScripts/Follower.py
@@ -14,12 +14,14 @@ class LineFollower:
         stream.start()
     
     def follow(self) -> None:
-        frameWidth = 240
+        frameWidth = 480
+        frameHeight = 240
+        halfFrameWidth = 240
+        halfFrameHeight = 120
         
         self.frame = stream.read()
-        #cv2.resize(self.frame,(679,453))
 
-        roi = self.frame[100:200,0:frameWidth]
+        #roi = self.frame[100:200,0:frameWidth] not being used rn
         
         self.line = cv2.inRange(self.frame,(0,0,0),(20,20,20))
         self.line = cv2.erode(self.line,None,iterations=2)
@@ -33,13 +35,12 @@ class LineFollower:
             if m['m00'] > 0:
                 xPos = int(m['m10']/m['m00'])
                 yPos = int(m['m01']/m['m00'])
-                error = (xPos-(frameWidth/2))/679*100
-                cv2.circle(roi,(xPos,yPos),5,(255,0,0),-1)
-                cv2.circle(self.frame,(679,906),5,(0,255,0),-1)
-                cv2.line(self.frame,(679,906),(xPos,yPos),(0,255,0),2)
-                opposite = xPos-679
-                adjacent = 906-yPos
-                angleRad = math.atan2(opposite, adjacent)
+                cv2.circle(self.frame,(xPos,yPos),5,(255,0,0),-1)
+                cv2.circle(self.frame,(halfFrameWidth,halfFrameWidth),5,(0,255,0),-1)
+                cv2.line(self.frame,(halfFrameWidth,halfFrameWidth),(xPos,yPos),(0,255,0),2)
+                opposite = halfFrameWidth-xPos
+                adjacent = frameHeight-yPos
+                angleRad = math.atan2(opposite,adjacent)
                 angle = angleRad*(180/3.14159)
                 print("angle = ",angle)
             else:
--- robotScripts/test.py
+++ robotScripts/test.py
@@ -1,13 +1,13 @@
-import cv2
+import math
 
-while True:
-    cap = cv2.VideoCapture(0)
-    ret, frame = cap.read()
-    
-    if ret:
-        cv2.imshow("Video Capture", frame)
-        
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-            break
-    else:
-        break
+# Define the coordinates of the two circles
+circle1_x, circle1_y = 0, 0  # Replace with the coordinates of your first circle
+circle2_x, circle2_y = -5, 5  # Replace with the coordinates of your second circle
+
+# Calculate the angle relative to the vertical line
+angle_rad = math.atan2(circle2_x - circle1_x, circle2_y - circle1_y)
+
+# Convert the angle from radians to degrees and reverse its sign
+angle_deg = -math.degrees(angle_rad)
+
+print(f"The angle relative to the vertical line is {angle_deg} degrees.")
